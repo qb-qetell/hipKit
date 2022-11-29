c93eb7d8349baa3681de4e0f54a99c00b56abf70
Brian Ibrahim Qamardeen <brian@qbqetell.one>
2022-11-29 14:13:43 +0100

    Im progress

--- va51_HttpIntf.go
+++ va51_HttpIntf.go
@@ -1,16 +1,20 @@
 package main
 
+import "crypto/tls"
 import "errors"
+import "fmt"
 import "net"
+import "regexp"
 
 type HttpIntf struct {
 	intfAdrsP1xx     string
 	intfAdrsP2xx        int
 	srvxQtxx            int
 	mssgScrtEnfrStts   bool
-	mssgScrtKeyx     []byte
-	mssgScrtCrtf     []byte
+	mssgScrtRsrc     tls.Config
 	mssgHndl         func (cnnc net.Conn)
+	shtdSgnl           bool
+	lifeStts           bool
 }
 	func HttpIntf_Estb ()   (*HttpIntf) {
 		return &HttpIntf {
@@ -18,26 +22,73 @@ type HttpIntf struct {
 			intfAdrsP2xx:             0,
 			srvxQtxx:                 0,
 			mssgScrtEnfrStts:     false,
-			mssgScrtKeyx:     []byte {},
-			mssgScrtCrtf:     []byte {},
+			shtdSgnl:             false,
+			shtdSgnl:             false,
 		}
 	}
-	func (i *HttpIntf) SetxIntfAdrs (p1xx string, p2xx int) (error) { return nil }
+	func (i *HttpIntf) SetxIntfAdrs (p1xx string, p2xx int) (error) {
+		_ba00 := net.ParseIP ("p1xx")
+		if _ba00 == nil {
+			_ca00 := fmt.Sprintf ("Interface Address Part 1 is invalid.")
+			return errors.New (_ca00)
+		}
+		/*--1--*/
+		if p2xx   <   1 {
+			_ca00 := fmt.Sprintf ("Interface Address Part 2 is invalid.")
+			return errors.New (_ca00)
+		}
+		/*--1--*/
+		i.intfAdrsP1xx = p1xx
+		i.intfAdrsP2xx = p2xx
+		/*--1--*/
+		return nil
+	}
 	func (i *HttpIntf) SetxSrvxQtxx (qtxx int) (error) {
 		if qtxx < 1 {
-			i.srvxQtxx = 0
-			return errors.New (
-				"Serving quota provided is less than 0: value must be a " +
-				"positive number.",
-			)
+			_ca00 := fmt.Sprintf ("Serving quota is invalid.")
+			return errors.New (_ca00)
 		}
 		return nil
 	}
-	func (i *HttpIntf) EnfrCnctScrt (keyx, crtf string) (error) { return nil }
-	func (i *HttpIntf) RlxxCnctScrt () {
-		i.mssgScrtEnfrStts =     false
-		i.mssgScrtKeyx     = []byte {}
-		i.mssgScrtCrtf     = []byte {}
+	func (i *HttpIntf) EnfrMssgScrt (name, keyx, crtf string) (error) {
+		_ba00, _bb00 := tls.LoadX509KeyPair (crtf, keyx)
+		/*--1--*/
+		if regexp.MustCompile (``).MatchString ("") == false {
+			_ca00 := fmt.Sprintf ("Name is invalid.")
+			return errors.New (_ca00)
+		}
+		/*--1--*/
+		if _bb00 != nil {
+			_ca00 := fmt.Sprintf ("Key and/or certficate are invalid.")
+			return errors.New (_ca00)
+		}
+		/*--1--*/
+		i.mssgScrtEnfrStts =  true
+		i.mssgScrtRsrc     = tls.Config {
+			Certificates: []tls.Certificate {_ba00},
+			ServerName:                       name ,
+		}
+		/*--1--*/
+		return nil
 	}
-	func (i *HttpIntf) Strt () (error, chan []string) { return nil, nil}
+	func (i *HttpIntf) RlxxMssgScrt () {
+		i.mssgScrtEnfrStts = false
+	}
+	func (i *HttpIntf) Actv () (error, chan []string) {
+		_ba00, _bb00 := net.Listen ("tcp", i.p1xx + ":" + i.p2xx)
+		if _bb00 != nil {
+			_ca00 := fmt.Sprintf ("%s", _bb00.Error ())
+			return errors.New (_ca00), nil
+		}
+		/*--1--*/
+		i.lifeStts = true
+		_bc00 := make (chan []string, 1)
+		_bc00 <- []string {"ba10"}
+		/*--1--*/
+		for {
+			
+		}
+		/*--1--*/
+		return nil, nil
+	} // close conn
 	func (i *HttpIntf) Halt ()  {}
--- vb01_Mssg.go
+++ vb01_Mssg.go
@@ -10,21 +10,29 @@ import "time"
 
 type Mssg struct {
 	core net.Conn
+	lsxx     bool
 }
-	func mssg_Estb ()  (*Mssg) {return &Mssg { core: nil }}
+	func mssg_Estb ()  (*Mssg) {return   &Mssg {  core: nil,   lsxx: false  }}
 	func (i *Mssg) ExtrCore () (net.Conn) { return i.core }
 	func (i *Mssg) Read (wndw ... time.Duration) (error, []byte, *http.Rqst) {
 		_bb00   := time.Second  * 10
 		if wndw != nil && len (wndw) > 0 { _bb00 = wndw [0] }
-		/*--1--*/
 		i.core.SetReadDeadline (time.Now ().Add (_bb00))
+		/*--1--*/
 		_bc00   := make ([]byte,  0)
 		for {
-			   _ca00 := make ([]byte, 1)
+			   _ca00 := make ([]byte, 1024)
 			_, _cb00 := i.core.Read (_ca00)
 			/*--2--*/
 			if        _cb00 != nil && _cb00 == io.EOF {
-				_bc00 = append (_bc00, _ca00...)
+				_da00 := 0;
+				for _db00 := 1; _db00 <= len (_ca00); _db00 ++ {
+					if _ca00 [_db00 - 1] == 0 {
+						break
+					}
+					_da00 = _db00
+				}
+				if _da00 != 0 { _bc00 = append (_bc00, _ca00 [0:_da00 + 1]...) }
 				break
 			} else if _cb00 != nil &&
 				errors.Is (_cb00, os.ErrDeadlineExceeded) == false {
